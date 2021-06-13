04026ccb03bd03a9d86cb19cf0bdbdaa282db5c0
Robert Maina <robert.maina@student.moringaschool.com>
2021-06-13 17:01:11 +0300

    added some arrays on the javascript

--- script.js
+++ script.js
@@ -15,8 +15,22 @@ function calculated(){
 
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+    var x = document.getElementById("inputGender").value;
+
     document.getElementById("day").innerHTML = "You were born on a "+ days[d.getDay()];
 
-    document.getElementById("kwasi").innerHTML = "Your Akan Name is " +femaleNames[d.getDay()];
+    if (x="Male"){
+      document.getElementById("kwasi").innerHTML = "Your Akan Name is " +maleNames[d.getDay()];
+    }
+    else if(x="Female") {
+      document.getElementById("kwasi").innerHTML = "Your Akan Name is " +femaleNames[d.getDay()];
+    }
+    else{
+      document.getElementById("kwasi").innerHTML = "Kindly select a your gender";
+    }
+
+    
+
+    
 }
 calculated();
